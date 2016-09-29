102c23329313d891953a096b51f86058468f299c
rex hsu <rexhsu@qnap.com>
2016-09-29 14:53:45 +0800

    a logic bug of 010 but leetcode doesn't find

--- 010.RegularExpressionMatching.c
+++ 010.RegularExpressionMatching.c
@@ -2,7 +2,7 @@ int slen, plen, plenm1;
 bool tryMatch(char *s, int sidx, char *p, int pidx) {
     if (pidx == plen) return sidx == slen;
     if ((pidx < plenm1 && p[pidx+1] != '*') || pidx == plenm1)
-        return (sidx < slen && s[sidx] == p[pidx] || p[pidx] == '.') && tryMatch(s, sidx+1, p, pidx+1);
+        return (sidx < slen && (s[sidx] == p[pidx] || p[pidx] == '.')) && tryMatch(s, sidx+1, p, pidx+1);
     while (s[sidx] == p[pidx] || p[pidx] == '.') {
         if (tryMatch(s, sidx, p, pidx+2)) return true;
         if (++sidx >= slen) break;
