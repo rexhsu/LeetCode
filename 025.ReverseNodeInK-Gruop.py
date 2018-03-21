dd4582f4800fe455e7ea6c42b58beadadcfd0477
rexhsu <justmake2003@yahoo.com.tw>
2018-03-21 11:32:25 +0800

    Give 025 a better solution.

--- 025.ReverseNodeInK-Gruop.py
+++ 025.ReverseNodeInK-Gruop.py
@@ -5,31 +5,21 @@
 #         self.next = None
 
 class Solution(object):
-    def reverseRange(self, start, end):
-        newhead = ListNode(0); newhead.next = start
-        while newhead.next != end:
-            tmp = start.next
-            start.next = tmp.next
-            tmp.next = newhead.next
-            newhead.next = tmp
-        return [end, start]
-
     def reverseKGroup(self, head, k):
         """
         :type head: ListNode
         :type k: int
         :rtype: ListNode
         """
-        if head == None: return head
-        nhead = ListNode(0)
-        nhead.next = head
-        start = nhead
-        while start.next:
-            end = start
-            for i in range(k - 1):
-                end = end.next
-                if end.next == None: return nhead.next
-            res = self.reverseRange(start.next, end.next)
-            start.next = res[0]
-            start = res[1]
-        return nhead.next
+        if k < 2: return head
+        node = head
+        for _ in range(k):
+            if not node: return head
+            node = node.next
+        prev = self.reverseKGroup(node, k)
+        for _ in range(k):
+            temp = head.next
+            head.next = prev
+            prev = head
+            head = temp
+        return prev
