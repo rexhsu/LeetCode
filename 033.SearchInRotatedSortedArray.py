7de54c5d813c1c31918c2d58332de0a39aaf4f51
rexhsu <justmake2003@yahoo.com.tw>
2018-03-31 11:44:11 +0800

    add 033.SearchInRotatedSortedArray.py

---
+++ 033.SearchInRotatedSortedArray.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        return nums.index(target) if target in nums else -1
