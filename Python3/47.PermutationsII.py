023f26d9d3e8d1833f8f208b38f616a582cc3364
Rex Hsu <rexhsu@qnap.com>
2022-04-21 19:31:25 +0800

    [47] less runtime by skip same element loop

--- Python3/47.PermutationsII.py
+++ Python3/47.PermutationsII.py
@@ -1,21 +1,23 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         used = [False]*len(nums)
-        def p(nums, s, cur, ans):
+        def per(nums, s, cur, ans):
             if s == len(nums):
-                print("append", cur)
-                if not cur in ans:
-                    ans.append(cur[:])
+                #print("append", cur)
+                #if not cur in ans: ans.append(cur[:])
+                ans.append(cur[:])
                 return
+            p = -11
             for i in range(len(nums)):
+                if nums[i] == p: continue
                 if used[i]: continue
                 used[i] = True
                 cur.append(nums[i])
-                print(i, cur)
-                p(nums, s+1, cur, ans)
-                cur.pop()
+                #print(i, cur)
+                per(nums, s+1, cur, ans)
+                p = cur.pop()
                 used[i] = False
         ans = []
-        cur = []
-        p(nums, 0, [], ans)
+        nums.sort()
+        per(nums, 0, [], ans)
         return ans
