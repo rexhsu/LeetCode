cf4232ff2100d25db2a8f6e22e0c29e324e33956
Rex Hsu <rexhsu@qnap.com>
2022-04-22 19:55:50 +0800

    [706] remove siz

--- Python3/706.DesignHashMap.go
+++ Python3/706.DesignHashMap.go
@@ -1,33 +1,30 @@
 type MyHashMap struct {
     array [][2]int
-    siz int
 }
 
 
 func Constructor() MyHashMap {
-    array := make([][2]int, 10000)
+    array := make([][2]int, 0, 10000)
     mhm := MyHashMap {
         array: array,
-        siz: 0,
     }
     return mhm
 }
 
 
 func (this *MyHashMap) Put(key int, value int)  {
-    for i,a := range(this.array[0:this.siz]) {
+    for i,a := range(this.array) {
         if a[0] == key {
             this.array[i][1] = value
             return
         }
     }
-    this.array[this.siz] = [2]int{key, value}
-    this.siz ++
+    this.array = append(this.array, [2]int{key, value})
 }
 
 
 func (this *MyHashMap) Get(key int) int {
-    for _,a := range(this.array[0:this.siz]) {
+    for _,a := range(this.array) {
         if a[0] == key {
             return a[1]
         }
@@ -36,14 +33,14 @@ func (this *MyHashMap) Get(key int) int {
     
 }
 
-
 func (this *MyHashMap) Remove(key int)  {
-    for i,a := range(this.array[0:this.siz]) {
+    for i,a := range(this.array) {
         if a[0] == key {
-            if i != this.siz - 1 {
-                this.array[i] = this.array[this.siz-1]
+            if i == len(this.array) - 1 { // tail of array
+                this.array = this.array[:i]
+            } else {
+                this.array = append(this.array[:i], this.array[(i+1):]...)
             }
-            this.siz--
         }
     }
 }
