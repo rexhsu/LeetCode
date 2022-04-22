105cb1239e9bdea725413bd4d9c99819677d3670
rexhsu <justmake2003@yahoo.com.tw>
2022-04-22 21:57:37 +0800

    [55] shorter version

--- Go/55.JumpGame.go
+++ Go/55.JumpGame.go
@@ -1,26 +1,18 @@
-func canJump(nums []int) bool {
-    i := 0
-    l := len(nums)
-    for i < (l - 1) { // stand before the last index
-        jump := nums[i]
-        if jump == 0 {
-            return false
-        }
-        cur := i+jump
-        farest := cur
-        if farest >= (l - 1) { // beyond the last
-            return true
-        }
-        //fmt.Println("for", i+1, i+jump)
-        for j:=i+1; j < cur ; j++ {
-            if j + nums[j] > farest {
-                farest = j + nums[j]
-            }
-        }
-        i = cur
-        if i + nums[i] < farest {
-            nums[i] = farest - i
-        }
+func max(x, y int) int {
+    if x > y {
+        return x
     }
-    return true
+    return y
+}
+
+func canJump(nums []int) bool {
+    goal  := len(nums) - 1
+    farest := 0
+    for i, n := range(nums) {
+        if farest < i || farest >= goal {
+            break
+        }
+        farest = max(i + n, farest)
+    }
+    return farest >= goal
 }
