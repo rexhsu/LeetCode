04ba82e1f19f95797c3aad20a0a983651211e93e
Rex Hsu <rexhsu@qnap.com>
2022-04-22 18:48:00 +0800

    [43] put caculation result in right position

--- Python3/43.MultiplyStrings.py
+++ Python3/43.MultiplyStrings.py
@@ -1,41 +1,24 @@
 class Solution:
-    def add(self, num1:str, num2: str) -> str:
-        if num1 == "": return num2
-        if num2 == "": return num1
-        rnum1, rnum2 = num1[::-1], num2[::-1]
-        l1, l2 = len(num1), len(num2)
-        maxlen = max(l1, l2)
-        out = ""
-        i, a = 0, 0
-        while i < maxlen:
-            n1, n2 = 0, 0
-            if i < l1: n1 = int(rnum1[i])
-            if i < l2: n2 = int(rnum2[i])
-            i += 1
-            s = n1 + n2 + a
-            a = s // 10
-            out += str(s % 10)
-        if a != 0: out += str(a)
-        #print("add", num1, num2, out[::-1])
-        
-        return out[::-1]
-        
     def multiply(self, num1: str, num2: str) -> str:
-        if num1 == "0" or num2 == "0": return "0"
+        if "0" in [num1, num2]: return "0"
+        
+        l1, l2 = len(num1), len(num2)
+        res = [0]*(l1 + l2)
         rnum1, rnum2 = num1[::-1], num2[::-1]
-        out = "" # global solution
-        i = 0
-        for c1 in rnum1:
-            a = 0
-            cur = "" # local solotion
-            for c2 in rnum2:
-                p = int(c1) * int(c2) + a
-                a = p // 10
-                cur += str(p % 10)
-                #print("cur", cur)
-            if a != 0: cur += str(a)
-            #print("cur", cur)
-            out = self.add(cur[::-1]+"0"*i, out)
-            i += 1
-            #print(out)
-        return out
+        
+        for i1 in range(l1):
+            for i2 in range(l2):
+                digit = int(rnum1[i1]) * int(rnum2[i2])
+                res[i1 + i2] += digit
+                res[i1 + i2 + 1] += (res[i1 + i2] // 10)
+                res[i1 + i2] = res[i1 + i2] % 10
+                #print(i1, i2, res)
+        
+        res, beg = res[::-1], 0
+        # count the leading zero length
+        while beg < len(res) and res[beg] == 0:
+            beg += 1
+        #print("res before map", res)
+        res = map(str, res[beg:])
+        
+        return "".join(res)
