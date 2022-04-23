bdecf0c5542e920009ec78da243434171e25c2f1
rexhsu <justmake2003@yahoo.com.tw>
2022-04-23 10:22:10 +0800

    Improve performance with bool arrays

--- Python3/1306.JumpGameIII.py
+++ Python3/1306.JumpGameIII.py
@@ -7,18 +7,19 @@ class Solution:
         left = start - arr[start]
         right = start + arr[start]
         #print("left", left, "right", right)
-        if left >= 0 and larr[start] == 0:
-            larr[start] = 1
+        if left >= 0 and not larr[start]:
+            larr[start] = True
             if self.reach(arr, larr, rarr, left):
                 return True
-        if right < len(arr) and rarr[start] == 0:
-            rarr[start] = 1
+        if right < len(arr) and not rarr[start]:
+            rarr[start] = True
             if self.reach(arr, larr, rarr, right):
                 return True
         return False
             
     def canReach(self, arr: List[int], start: int) -> bool:
         l = len(arr)
-        larr = [0]*l
-        rarr = [0]*l
+        larr = [False]*l
+        rarr = [False]*l
         return self.reach(arr, larr, rarr, start)
+        
