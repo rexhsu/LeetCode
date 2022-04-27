371baf1ed735f5dd627466999524c4df9ee9814d
Rex Hsu <rexhsu@qnap.com>
2022-04-27 16:35:10 +0800

    [53] add position handle comments

--- Python3/53.MaximumSubarray.py
+++ Python3/53.MaximumSubarray.py
@@ -2,13 +2,17 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
         cur, res = nums[0], nums[0]
-        for num in nums[1:]:
-            if cur < 0: # i-1 contribute nothing: case1
+        curL, curR, resL, resR = 0, 0, 0, 0
+        for idx, num in enumerate(nums[1:]):
+            if cur < 0: # i-1 contribute nothing, start from i
                 cur = num
-            else:
-                cur += num # case2
-            res = max(res, cur) 
-            # if res update because case1, l=r=current index
-            # if res update because case2, r=current index
+                #curL, curR = idx+1, idx+1
+            else: # i-1 contributes, include i
+                cur += num
+                #curR = idx+1
+            if cur > res:
+                res = cur
+                #resL, resR = curL, curR
             
+        #print(resL, resR)
         return res
