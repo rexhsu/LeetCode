43d2d4335943e14eb7169d9cee7fd414cda88478
Rex Hsu <rexhsu@qnap.com>
2022-04-29 20:51:26 +0800

    [785] improve without set

--- Python3/785.IsGraphBipartite.py
+++ Python3/785.IsGraphBipartite.py
@@ -2,43 +2,37 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         siz = len(graph)
         
-        set1 = set()
-        set2 = set()
-        
+        grp = [2]*siz # 2 means not group yet, we have only 2 group: 0 & 1
+
         que = collections.deque()
-        handle = 0
-        
+        handle = 0 # record this level we have to handle
         for i in range(siz):
             if graph[i]:
                 handle += 1
                 que.append(i)
-            
-        set1.add(0)
+        grp[0] = 0
         level = 0
         
         while que:
             for _ in range(handle): # just handle this level
                 i = que.popleft()
                 handle -= 1
-
-                if i in set1:
+                
+                if grp[i] != 2:
                     for n in graph[i]:
-                        if n in set1: return False
-                        else: set2.add(n)
-                elif i in set2:
-                     for n in graph[i]:
-                        if n in set2: return False
-                        else: set1.add(n)
+                        if grp[n] == grp[i]: return False
+                        else: grp[n] = (1 - grp[i])
                 else:
                     for n in graph[i]:
-                        if n in set1: set2.add(i)
-                        if n in set2: set1.add(i)
-                    if i == level: # this level must put in a group, randomly assign to set1
-                        set1.add(i)
+                        if grp[n] != 2:
+                            grp[i] = (1 - grp[n]) # assign to opposite group
+                            break
+                    if grp[i] == 2 and i == level: # i at this level must enter group, randomly assign to group 0
+                        grp[i] = 0
                     handle += 1 # leave i to next level
                     que.append(i)
             level += 1
-            #print("que", que, "s1 s2", set1, set2, "lv, handle", level, handle)  
+            print("que", que, "grp", grp, "lv, handle", level, handle)  
         
         return True
             
