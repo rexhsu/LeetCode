4ccbab85cba7be9f53a6be74d1cd245387a2efe2
rexhsu <justmake2003@yahoo.com.tw>
2022-05-02 16:25:32 +0800

    Time: 4 ms (93.82%), Space: 4.2 MB (51.17%) - LeetHub

---
+++ 1-two-sum/1-two-sum.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i, v := range nums {
+		j, ok := m[v]
+		if ok {
+            return []int{j, i}
+        } else {
+            m[target-v] = i
+        }
+
+	}
+	return nil
+}
