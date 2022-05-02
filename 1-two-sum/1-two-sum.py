520dc65a69b116ee6aef70f7a81bce2072c7a060
rexhsu <justmake2003@yahoo.com.tw>
2022-05-02 16:55:33 +0800

    Time: 196 ms (37.24%), Space: 15.5 MB (9.56%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,13 +1,23 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        remains = [(target - i) for i in nums]
-        #print(remains)
- 
-        for i, r in enumerate(remains):
-            for j, n in enumerate(nums):
-                if i!=j and r==n: return [i, j]
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexedNums = [(i, n) for i, n in enumerate(nums)]
+        indexedNums = sorted(indexedNums, key=lambda x: x[1])
+        #print(indexedNums)
+        
+        def bsrch(inums, target):
+            left, right = 0, len(inums)-1
+            while left <= right:
+                mid = int((left + right) / 2)
+                if target < inums[mid][1]:
+                    right = mid - 1
+                elif target > inums[mid][1]:
+                    left = mid + 1
+                else:
+                    return inums[mid][0]
+            return -1
+        
+        for i, n in indexedNums:
+            j = bsrch(indexedNums, target - n)
+            if j != -1 and j != i:
+                return [i, j]
+            
