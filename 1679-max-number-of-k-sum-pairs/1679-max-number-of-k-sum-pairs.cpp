675db31b1f75656d291b8723929ab46f51760ba6
rexhsu <justmake2003@yahoo.com.tw>
2022-05-04 17:13:00 +0800

    Time: 232 ms (31.38%), Space: 60.9 MB (57.64%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
+    bool predicate(int element, int k) {
+        return (element < k);
+    }
     int maxOperations(vector<int>& nums, int k) {
-        // sort
-        sort(nums.begin(), nums.end());
+        vector<int> fnums(nums.size());
+        copy_if(nums.begin(), nums.end(), fnums.begin(), [=](int em) {
+            return predicate(em, k);
+        });
         
+        // sort
+        sort(fnums.begin(), fnums.end());
+        /* debug
+        for_each(fnums.begin(), fnums.end(),
+                  [](const int& n) { cout << n << " "; });
+        cout << endl;
+        */
+
         int sol = 0;
         int i = 0;
-        int j = nums.size() - 1;
+        int j = fnums.size() - 1;
         while (i<j) {
-            if (nums[j] > k or nums[i] + nums[j] > k) {
+            if (fnums[j] > k or fnums[i] + fnums[j] > k) {
                 j--;
-            } else if (nums[i] + nums[j] < k) {
+            } else if (fnums[i] + fnums[j] < k) {
                 i++;
             } else {
                 sol++;
