bb8f772383eaf954e0326a3cdc657e92ac007121
rexhsu <justmake2003@yahoo.com.tw>
2022-05-04 15:39:09 +0800

    Time: 1088 ms (19.20%), Space: 26.1 MB (97.32%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -3,21 +3,18 @@ class Solution:
         """
         1 <= nums[i] <= 10^9
         """
-        arr = [i for i in nums if i < k]
-        print(arr)
+        arr = [i for i in nums if i < k] # filter larger than k. O(n)
+        arr = sorted(arr) # O(nlogn)
+        #print(arr)
         
-        arr = sorted(arr)
-        
-        sol = 0
-        i, j = 0, len(arr)-1
-        while i<j:
+        sol, i, j = 0, 0, len(arr)-1
+        while i<j: # O(n)
             if arr[i] + arr[j] > k:
                 j-=1
             elif arr[i] + arr[j] < k:
                 i+=1
             else:
-                sol += 1
-                i, j = i+1, j-1
+                sol, i, j = sol+1, i+1, j-1
                 
         return sol
             
