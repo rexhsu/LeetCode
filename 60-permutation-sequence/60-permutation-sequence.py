db2fcb06daca2863a923dad72625a5ead4b5dea1
rexhsu <justmake2003@yahoo.com.tw>
2022-05-05 17:32:39 +0800

    Time: 57 ms (34.67%), Space: 13.9 MB (80.05%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.py
+++ 60-permutation-sequence/60-permutation-sequence.py
@@ -2,13 +2,15 @@ class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         if n==1: return  "1"
         num = [str(i) for i in range(1, n+1)]
+        fac = [1]
+        for i in range(1, n-1):
+            fac.append((i+1)*fac[i-1])
         sol = []
         k -= 1 # index from 0
-        for i in range(n-1, 0, -1):
-            f = math.factorial(i)
-            idx = int(k/math.factorial(i))
-            #print("k i f idx", k, i, f, idx)
-            sol.append(num.pop(idx))
+        for i in range(n-2, -1, -1):
+            #f = math.factorial(i)
+            f = fac[i]
+            sol.append(num.pop(int(k/f)))
             k = k%f
         sol.append(num.pop())
         #print(num, sol, ''.join(sol))
